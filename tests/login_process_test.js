95d7d3277b7e592666537a5a965d8b2e03ca1000
Metin Özel <154251546+metinozell@users.noreply.github.com>
2026-03-12 13:23:27 +0300

    test: Negatif login (yanlis sifre) senaryosu eklendi.

--- tests/login_process_test.js
+++ tests/login_process_test.js
@@ -5,3 +5,9 @@ Scenario('Basarili bir sekilde sisteme giris yapilabilmeli',  ({ I, LoginPage })
     LoginPage.login('standard_user', 'secret_sauce');
     I.see('Products');
 });
+
+Scenario('Yanlis sifre ile giris yapilamamali', ({ I, LoginPage }) => {
+    I.amOnPage('/');
+    LoginPage.login('standard_user', 'wrong_password');
+    I.see('Epic sadface: Username and password do not match any user in this service');
+});
